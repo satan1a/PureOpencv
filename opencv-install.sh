c474b7d66610420ab4a5fae51b00f9c1da9e1cf0
hotsauce1861 <zhaojunhui1861@163.com>
2018-08-04 00:14:34 +0800

    update opencv-install.sh

--- opencv-install.sh
+++ opencv-install.sh
@@ -1,31 +1,35 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt get install -y build-essential
-sudo apt get install -y cmake
-sudo apt get install -y cmake-qt-gui
+#sudo apt-get update
+sudo apt-get install -y build-essential
+sudo apt-get install -y cmake
+sudo apt-get install -y cmake-qt-gui
 sudo apt-get install -y libavcodec-dev libavformat-dev libswscale-dev
 sudo apt-get install -y libjpeg-dev libpng-dev libtiff-dev libjasper-dev
 if [ ! -f 3.4.2.zip ];then
 	sudo wget https://github.com/opencv/opencv/archive/3.4.2.zip
 fi
-sudo unzip -y  3.4.2.zip && cd $(ls | grep *3.4.2)
-
-sudo mkdir opencv-build
-cd opencv-build
+sudo unzip -n 3.4.2.zip 
+if [ ! -f opencv-build ];then
+	mkdir opencv-build
+fi
 
 echo "cmake or cmake-gui config this source?"
 echo "1.cmake"
 echo "2.cmake-gui"
-
 read SELECT
 
+echo "please input opence source path:"
+echo $(ls | grep "opencv*")
+echo "please your soure path:"
+read CVPATH
+
 if [ $SELECT -eq 1 ];then
-	cmake ../
+	cd opencv-build && cmake ../$CVPATH/
 elif [ $SELECT -eq 2 ];then
-	cmake-gui ../
+	cd opencv-build && cmake-gui ../$CVPATH/
 fi
 
-make -j8 
-echo $(make install)
+make -j8
+make install
 
 echo "install success"
