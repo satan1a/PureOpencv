ebddc11e4e529183ea88b79bff6cdc0eba38a7af
hotsauce1861 <zhaojunhui1861@163.com>
2018-08-05 22:42:44 +0800

    add eye mat

--- src/image_smooth/image_smooth.cpp
+++ src/image_smooth/image_smooth.cpp
@@ -6,13 +6,17 @@ using namespace cv;
 
 int main(char argc,char** argv){
 
-    Mat ori_img = imread(argv[1],-1);
+    Mat ori_img = imread(argv[1],CV_LOAD_IMAGE_COLOR);
     Mat dst_img;
 
-    int r = 50;
+    int r = 1000;
 
     Mat kernel(r,r,CV_64FC1,1);
     kernel = kernel/(r*r);
+
+
+    Mat none = Mat::eye(r,r,CV_64F);
+    none = none/r;
 
     if(ori_img.empty()){
         return -1;
@@ -21,7 +25,8 @@ int main(char argc,char** argv){
     cv::namedWindow("imagesmooth",cv::WINDOW_NORMAL);
     cv::namedWindow("imagesmooth_out",cv::WINDOW_NORMAL);
 
-    filter2D(ori_img,dst_img,-1,kernel);
+    filter2D(ori_img,dst_img,-1,none);
+    //filter2D(ori_img,dst_img,-1,kernel);
     //filter2D(ori_img,dst_img,kernel);
     cv::imshow("imagesmooth",ori_img);
     cv::imshow("imagesmooth_out",dst_img);
